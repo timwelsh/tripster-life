ca61620e38688823391e90bee08f8cc9e74340c6
leaf-junkie <ragsdale.jar@gmail.com>
2019-05-16 19:16:29 -0400

    fix tim's pic

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -6,41 +6,42 @@ const style = {
 
 class Contact extends Component {
     render() {
+        // const { handleSubmit } = this.props;
         return (
             <div>
-                <div class="row">
-                    <form class="col s12">
+                <div className="row">
+                    <form /* onSubmit={handleSubmit(this.onSubmit)} */ className="col s12">
                         <h4>Send us a message</h4>
                         <fieldset>
-                            <div class="row">
-                                <div class="input-field col s6">
-                                <input id="first_name" type="text" class="validate" />
+                            <div className="row">
+                                <div className="input-field col s6">
+                                <input id="first_name" type="text" className="validate" />
                                 <label for="first_name">First Name</label>
                                 </div>
-                                <div class="input-field col s6">
-                                <input id="last_name" type="text" class="validate" />
+                                <div className="input-field col s6">
+                                <input id="last_name" type="text" className="validate" />
                                 <label for="last_name">Last Name</label>
                                 </div>
                             </div>
-                            <div class="row">
-                                <div class="input-field col s12">
-                                <input id="email" type="email" class="validate" />
+                            <div className="row">
+                                <div className="input-field col s12">
+                                <input id="email" type="email" className="validate" />
                                 <label for="email">Email</label>
                                 </div>
                             </div>
-                            <div class="row">
-                                <div class="input-field col s12">
-                                <textarea id="subject" type="text" class="materialize-textarea" />
+                            <div className="row">
+                                <div className="input-field col s12">
+                                <textarea id="subject" type="text" className="materialize-textarea" />
                                 <label for="subject">Subject</label>
                                 </div>
                             </div>
-                            <div class="row">
-                                <div class="input-field col s12">
-                                <textarea id="message" type="text" class="materialize-textarea" />
+                            <div className="row">
+                                <div className="input-field col s12">
+                                <textarea id="message" type="text" className="materialize-textarea" />
                                 <label for="message">Message</label>
                                 </div>
                             </div>
-                            <a class="waves-effect waves-light btn">Submit</a>
+                            <button type="submit" className="btn btn-primary" style={{marginTop: "1rem"}}>Sign Up</button>
                         </fieldset>
                     </form>
                 </div>

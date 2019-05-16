a7b721243c5837c27ecc9ac05f30e6d72218a8cc
Tim Welsh <tim@timwelsh.com>
2019-05-16 19:00:25 -0400

    build changes

--- server/app.js
+++ server/app.js
@@ -3,7 +3,9 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const nodemailer = require('nodemailer');
+const path = require('path');
+// const nodemailer = require('nodemailer');
+
 
 mongoose.Promise = global.Promise;
 if (process.env.NODE_ENV === 'test') {
@@ -13,6 +15,9 @@ if (process.env.NODE_ENV === 'test') {
 }
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "../client/build/")))
+
 app.use(cors());
 
 // Middlewares moved morgan into if for clear tests
@@ -24,33 +29,36 @@ app.use(bodyParser.json());
 
 // Routes
 app.use('/users', require('./routes/users'));
-
+app.use("*", function(req, res) {
+  console.log(path.join(__dirname, "../client/build/index.html"))
+  res.sendFile(path.join(__dirname, "../client/build/index.html"))
+})
 // Nodemailer
-async function mail() {
+// async function mail() {
 
-  // Creates a reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: '',
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: 'ragsdale.jar@gmail.com',
-      pass: 'test1234',
-    }
-  });
+//   // Creates a reusable transporter object using the default SMTP transport
+//   let transporter = nodemailer.createTransport({
+//     host: '',
+//     port: 587,
+//     secure: false, // true for 465, false for other ports
+//     auth: {
+//       user: 'ragsdale.jar@gmail.com',
+//       pass: 'test1234',
+//     }
+//   });
 
-  // Send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: ' "Tripster Life" <admin@tripster.life> ', //sender address
-    to: 'admin@tripster.life, ragsdale.jar@gmail.com', // receiver's address
-    subject: 'Hello', // subject line
-    text: 'Hello, world!', // plain text body
-    html: '<p>Hello, world!</p>' // html body
-  });
+//   // Send mail with defined transport object
+//   let info = await transporter.sendMail({
+//     from: ' "Tripster Life" <admin@tripster.life> ', //sender address
+//     to: 'admin@tripster.life, ragsdale.jar@gmail.com', // receiver's address
+//     subject: 'Hello', // subject line
+//     text: 'Hello, world!', // plain text body
+//     html: '<p>Hello, world!</p>' // html body
+//   });
 
-  console.log('Message sent: %s', info.messageId);
-}
+//   console.log('Message sent: %s', info.messageId);
+// }
 
-mail().catch(console.error);
+// mail().catch(console.error);
 
 module.exports = app;

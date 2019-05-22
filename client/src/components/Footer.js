34a0bb09f2613c9352fb52380a7e5753f0ad1bee
leaf-junkie <ragsdale.jar@gmail.com>
2019-05-22 14:12:02 -0400

    added pronunciation to footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -13,7 +13,7 @@ class Footer extends Component {
             }}>
                 <div>
                     <div className='col s8 m8 l8'>
-                        <p>Tripster:</p>
+                        <p>Tripster ( /trĭp · stɜr/ ):</p>
                         <p>A traveler who likes visiting places just because they are off the beaten path and/or lack tourist infrastructure. They often complain about crowds or too many people being there before, but ironically want to go obscure travel destinations just to brag on their social media about being different or edgy in vacation choices.                        </p>
                     </div>
                     <div className='col s3 m3 l3 offset-s1 offset-m1 offset-l1'>

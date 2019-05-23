1c99b6b91e59b9fad115fe1e8a7fb54c5d81f3de
ericksont11 <ericksont91@gmail.com>
2019-05-23 00:02:14 -0400

    try sport

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 console.log('APP ', app)
 // Start the server
-const port = process.env.PORT || 5000;
+const port = process.env.SPORT || 5000;
 app.listen(port);
 console.log(`Server listening at ${port}`);
 // refactored code for easier test and feature scale
